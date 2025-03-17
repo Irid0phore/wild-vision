bc373799f06b1143801731a17edd34ca557512cf
Irid0phore <RFJdh=iq3MyE19nRbB%5>
2025-03-17 14:34:54 -0400

    Added set_message_interval() function

--- MavProxy/pymavlink_test.py
+++ MavProxy/pymavlink_test.py
@@ -1,35 +1,35 @@
 from pymavlink import mavutil
 
+
+def set_message_interval(connection, msg_id, interval_us):
+	# Define command_long_encode message to send MAV_CMD_SET_MESSAGE_INTERVAL command
+	# param1: Message to stream
+	# param2: Stream interval in microseconds
+	message = connection.mav.command_long_encode(
+		connection.target_system,  # Target system ID
+		connection.target_component,  # Target component ID
+		mavutil.mavlink.MAV_CMD_SET_MESSAGE_INTERVAL,  # ID of command to send
+		0,  # Confirmation
+		msg_id,  # param1: Message ID to be streamed
+		interval_us, # param2: Interval in microseconds
+		)
+	# Send the COMMAND_LONG
+	connection.mav.send(message)
+
+	# Wait for a response (blocking) to the MAV_CMD_SET_MESSAGE_INTERVAL command and print result
+	response = connection.recv_match(type='COMMAND_ACK', blocking=True)
+	if response:
+		if response.command == mavutil.mavlink.MAV_CMD_SET_MESSAGE_INTERVAL:
+			if response.result == mavutil.mavlink.MAV_RESULT_ACCEPTED:
+				return True
+	return False
+
 # Start a connection listening on a UDP port
 connection = mavutil.mavlink_connection('udpin:localhost:5762')
 
 # Wait for the first heartbeat to set the system and component ID of remote system for the link
 connection.wait_heartbeat()
-print("Heartbeat from system (system %u component %u)" % (connection.target_system, connection.target_component))
+print(f"Heartbeat from system (system {connection.target_system} component {connection.target_component})")
 
-# Define command_long_encode message to send MAV_CMD_SET_MESSAGE_INTERVAL command
-# param1: MAVLINK_MSG_ID_BATTERY_STATUS (message to stream)
-# param2: 1000000 (Stream interval in microseconds)
-message = connection.mav.command_long_encode(
-        connection.target_system,  # Target system ID
-        connection.target_component,  # Target component ID
-        mavutil.mavlink.MAV_CMD_SET_MESSAGE_INTERVAL,  # ID of command to send
-        0,  # Confirmation
-        mavutil.mavlink.MAVLINK_MSG_ID_BATTERY_STATUS,  # param1: Message ID to be streamed
-        1000000, # param2: Interval in microseconds
-        0,       # param3 (unused)
-        0,       # param4 (unused)
-        0,       # param5 (unused)
-        0,       # param5 (unused)
-        0        # param6 (unused)
-        )
-
-# Send the COMMAND_LONG
-connection.mav.send(message)
-
-# Wait for a response (blocking) to the MAV_CMD_SET_MESSAGE_INTERVAL command and print result
-response = connection.recv_match(type='COMMAND_ACK', blocking=True)
-if response and response.command == mavutil.mavlink.MAV_CMD_SET_MESSAGE_INTERVAL and response.result == mavutil.mavlink.MAV_RESULT_ACCEPTED:
-    print("Command accepted")
-else:
-    print("Command failed")
+result = set_message_interval(connection, mavutil.mavlink.MAVLINK_MSG_ID_BATTERY_STATUS, 1e6)
+print("Command accepted" if result else "Command failed")
