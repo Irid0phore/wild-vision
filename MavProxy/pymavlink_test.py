04bef117587761442aa11ab47efae3f86af3f6db
Irid0phore <RFJdh=iq3MyE19nRbB%5>
2025-03-17 15:38:51 -0400

    Fixed pymavlink_test.py

--- MavProxy/pymavlink_test.py
+++ MavProxy/pymavlink_test.py
@@ -1,10 +1,32 @@
 from pymavlink import mavutil
 
 
+def request_message(connection, msg_id):
+	message = connection.mav.command_long_encode(
+		connection.target_system,  # Target system ID
+		connection.target_component,  # Target component ID
+		mavutil.mavlink.MAV_CMD_REQUEST_MESSAGE,  # ID of command to send
+		0,  # Confirmation
+		msg_id,  # param1: Message ID to be requested
+		0, # param2: Unused
+		0, # param3: Unused
+		0, # param4: Unused
+		0, # param5: Unused
+		0, # param6: Unused
+		0, # param7: Unused
+		)
+	# Send the COMMAND_LONG
+	connection.mav.send(message)
+
+	# Wait for a response (blocking) to the MAV_CMD_SET_MESSAGE_INTERVAL command and print result
+	response = connection.recv_match(type='COMMAND_ACK', blocking=True)
+	if response:
+		if response.command == mavutil.mavlink.MAV_CMD_REQUEST_MESSAGE:
+			if response.result == mavutil.mavlink.MAV_RESULT_ACCEPTED:
+				return True
+	return False
+
 def set_message_interval(connection, msg_id, interval_us):
-	# Define command_long_encode message to send MAV_CMD_SET_MESSAGE_INTERVAL command
-	# param1: Message to stream
-	# param2: Stream interval in microseconds
 	message = connection.mav.command_long_encode(
 		connection.target_system,  # Target system ID
 		connection.target_component,  # Target component ID
@@ -12,6 +34,11 @@ def set_message_interval(connection, msg_id, interval_us):
 		0,  # Confirmation
 		msg_id,  # param1: Message ID to be streamed
 		interval_us, # param2: Interval in microseconds
+		0, # param3: Unused
+		0, # param4: Unused
+		0, # param5: Unused
+		0, # param6: Unused
+		0, # param7: Unused
 		)
 	# Send the COMMAND_LONG
 	connection.mav.send(message)
@@ -25,7 +52,7 @@ def set_message_interval(connection, msg_id, interval_us):
 	return False
 
 # Start a connection listening on a UDP port
-connection = mavutil.mavlink_connection('udpin:localhost:5762')
+connection = mavutil.mavlink_connection('/dev/AMC0')
 
 # Wait for the first heartbeat to set the system and component ID of remote system for the link
 connection.wait_heartbeat()
@@ -34,14 +61,15 @@ print(f"Heartbeat from system (system {connection.target_system} component {conn
 result = set_message_interval(connection, mavutil.mavlink.MAVLINK_MSG_ID_SYS_STATUS, 1e6)
 print("Command accepted" if result else "Command failed")
 
-msg = m.recv_match(type='SYS_STATUS',blocking=True)
-if not msg:
-	exit()
-if msg.get_type() == "BAD_DATA":
-	if mavutil.all_printable(msg.data):
-		sys.stdout.write(msg.data)
-		sys.stdout.flush()
-else:
-	#Message is valid
-	# Use the attribute
-	print('Mode: %s' % msg.mode)
+
+while True:
+	msg = connection.recv_match(blocking=True)
+	if not msg:
+		pass
+	elif msg.get_type() == "BAD_DATA":
+		if mavutil.all_printable(msg.data):
+			sys.stdout.write(msg.data)
+			sys.stdout.flush()
+	else:
+		#Message is valid
+		print(msg)
